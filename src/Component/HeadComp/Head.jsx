0720aec2d86b1e15f1dda3315066c6fb8da9c487
unknown <danielawa53@gmail.com>
2025-08-13 15:51:25 +0100

    updated nav

--- src/Component/HeadComp/Head.jsx
+++ src/Component/HeadComp/Head.jsx
@@ -1,39 +1,45 @@
-import "./Head.css"
-import Logo from "../../assets/Logo.svg"
-import Menu from "../../assets/menuu.svg"
-import Button from "../Button"
-import { Link } from "react-router-dom"
+
+import React, { useState } from "react";
+import "./Head.css";
+import Logo from "../../assets/Logo.svg";
+import Menu from "../../assets/menuu.svg";
+import Button from "../Button";
+import { Link } from "react-router-dom";
 
 const Head = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  return (
+    const handleMenuClick = (e) => {
+        e.preventDefault();
+        setMenuOpen((prev) => !prev);
+    };
 
+    return (
         <nav>
             <div className="logo">
-            <img src={Logo} alt=""/>
-                <p> Gual </p> 
+                <img src={Logo} alt="" />
+                <p> Gual </p>
             </div>
 
-            <ul>
+            <ul className={menuOpen ? "active" : ""}>
                 <li><Link to="/">Home</Link></li>
-                {/* <li><a href="#">Home</a></li> */} 
                 <li><Link to="/pill">PillPack</Link></li>
                 <li><Link to="/">Health</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
             <div className="btn">
-                {/* ADDING A BUTTON PROP */}
-                <Button btnName='Sign Up | Sign In' /> 
+                <Button btnName='Sign Up | Sign In' />
             </div>
-            <li className="menu"><a href="#"> 
-                <img src={Menu} alt=""/>
-            </a></li>
+            <li className="menu">
+                <a href="#" onClick={handleMenuClick}>
+                    <img src={Menu} alt="Menu" />
+                </a>
+            </li>
         </nav>
-        
-  )
-}
+    );
+};
 
-export default Head
+export default Head;
 
 
 
